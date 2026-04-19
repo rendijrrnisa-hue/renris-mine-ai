f6de51a9510c6f07b1aa03cc13787f05842bb303
rendijrrnisa-hue <rendijrrnisa@gmail.com>
2026-04-19 21:07:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,8 @@ blockchain.push(minedBlock)
 return minedBlock
 
 }
-block.hash = crypto.createHash("sha256")
+block.hash = 
+crypto.createHash("sha256")
 .update(JSON.stringify(block))
 .digest("hex")
 
