f084f0482fc56c2b589656dd485d7152a3598014
rendijrrnisa-hue <rendijrrnisa@gmail.com>
2026-04-19 21:11:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -109,6 +109,8 @@ res.json({balance})
 })
 
 
-app.listen(3000,()=>{
-console.log("RAI Blockchain Running 3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+console.log("Server running on port " + PORT)
 })
